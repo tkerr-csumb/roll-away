274798f9c1fc9ca7c64f3e433bfe650632363735
Robyn Felton <rfelton@csumb.edu>
2026-04-14 17:19:57 -0700

    Feat: Switch player movement to use InputActions wrapper
    Replaces the PlayerInput component with a programmatic implementation using the source-generated InputActions class. This also simplifies the input actions asset by removing unused default actions, and it adds controller support.

--- rollAway-project/Assets/Scripts/PlayerController.cs
+++ rollAway-project/Assets/Scripts/PlayerController.cs
@@ -1,59 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+    public static PlayerController Instance { get; private set; }
     private Rigidbody rb;
-    private float movementX;
-    private float movementY;
+    private Vector2 movementInput;
     public float speed;
     public TextMeshProUGUI countText;
     private int count;
     public GameObject winTextObject;
+    private InputActions inputActions;
+
+    void Awake()
+    {
+        Instance = this;
+        inputActions = new InputActions();
+    }
+
+    void OnEnable()
+    {
+        inputActions.Player.Move.performed += OnMove;
+        inputActions.Player.Move.canceled += OnMove;
+        inputActions.Player.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputActions.Player.Move.performed -= OnMove;
+        inputActions.Player.Move.canceled -= OnMove;
+        inputActions.Player.Disable();
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start(){
-     rb = GetComponent<Rigidbody>();   
-     count = 0;
-     SetCountText();
-     winTextObject.SetActive(false);
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
-    void OnMove(InputValue movementValue){
-        Vector2 movementVector = movementValue.Get<Vector2>();
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+
+    void OnMove(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
     }
-    void SetCountText() 
-   {
-       countText.text =  "Polyhedrons: " + count.ToString();
-       if (count >= 12)
-       {
-           winTextObject.SetActive(true);
-           Destroy(GameObject.FindGameObjectWithTag("Enemy"));
-       }
-   }
-    void FixedUpdate(){
-        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+
+    void SetCountText()
+    {
+        countText.text = "Polyhedrons: " + count.ToString();
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementInput.x, 0.0f, movementInput.y);
         rb.AddForce(movement * speed);
     }
-    void OnTriggerEnter(Collider other){
-        if (other.gameObject.CompareTag("PickUp")){
-        other.gameObject.SetActive(false);
-        count = count +1;
-        SetCountText();
-       }
-   }
-   private void OnCollisionEnter(Collision collision)
-{
-   if (collision.gameObject.CompareTag("Enemy"))
-   {
-       // Destroy the current object
-       Destroy(gameObject); 
-       winTextObject.gameObject.SetActive(true);
-       winTextObject.GetComponent<TextMeshProUGUI>().text = "HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA";
-   }
-}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // Destroy the current object
+            Destroy(gameObject);
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text =
+                "HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA";
+        }
+    }
 }
