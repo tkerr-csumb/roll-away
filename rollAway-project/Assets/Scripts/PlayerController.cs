2e672ef03f348d96a0cfc5719e89a995718b48a8
liamsmith-otter <liamsmith@csumb.edu>
2026-04-21 15:16:54 -0700

    Added flypaper.
    only left I want to add to it is the ability to slowly climb sheer walls from a stop.

--- rollAway-project/Assets/Scripts/PlayerController.cs
+++ rollAway-project/Assets/Scripts/PlayerController.cs
@@ -13,16 +13,21 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float jumpPower = 7f;
     public float dashPower = 5f;
-    public float bounceBoost = 10f;
+    public float bounceBoost = 14f;
     public TextMeshProUGUI countText;
     private int count;
+    private float maxSpeed = 11.5f;
     public GameObject winTextObject;
     private bool hasBurstCharge = true;
     private InputActions inputActions;
     private bool isGrounded = true;
-    public bool onIce = false;
+    [NonSerialized] public bool onIce = false;
+    public float stickyMoveMultiplier = 4f;
+    public float stickyClimbForce = 25f;
     private bool onRubber = false;
-
+    private bool onSticky = false;
+    private Vector3 stickyNormal;
+    
     void Awake()
     {
         Instance = this;
@@ -126,13 +131,46 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementInput.x, 0.0f, movementInput.y);
         // change behavior of player movement if on ice (more slidey)
-        if(onIce)
+        if (onSticky)
         {
-            rb.linearDamping = 0.05f; // very low = slidey
+            rb.linearDamping = 2f;
+            rb.angularDamping = 2f;
+            // Wall climbing (no cielings)
+            if (stickyNormal.y < 0.5f && stickyNormal.y > -0.5f)
+            {
+                Vector3 climbingMovement = Vector3.ProjectOnPlane(movement, stickyNormal);
+                //keep ball attached and not fall off
+                rb.AddForce(-stickyNormal*20f, ForceMode.Force);
+                // offset gravity while climbing (not all the way though)
+                rb.AddForce(Vector3.up *9.81f, ForceMode.Force);
+                Vector3 climbDir = climbingMovement.normalized;
+                // rb.AddForce(climbDir * stickyClimbForce, ForceMode.Acceleration);
+                rb.AddForce(climbingMovement * (speed * stickyMoveMultiplier), ForceMode.Force);
+                return;
+            }
+        }
+        else if (onIce)
+        {
+            speed += 0.1f * Time.deltaTime;
+
+            rb.linearDamping = 0.05f;
+            rb.angularDamping = 0.05f;
+            // Apply movement
+            rb.AddForce(movement * speed, ForceMode.Force);
+
+            //clamp velocity
+            if (rb.linearVelocity.magnitude > maxSpeed)
+            {
+                rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, maxSpeed);
+            }
+
+            return;
         }
         else
         {
+            speed = 10f;
             rb.linearDamping = 1f; // normal
+            // rb.angularDamping = 1f;
         }
         rb.AddForce(movement * speed);
     }
@@ -157,10 +195,9 @@ public class PlayerController : MonoBehaviour
             winTextObject.GetComponent<TextMeshProUGUI>().text =
                 "HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA";
         }
-
+        Vector3 normal = collision.contacts[0].normal;
         if (collision.gameObject.CompareTag("Ground"))
         {
-            Vector3 normal = collision.contacts[0].normal;
 
             // Surface has to face up enough to be floor for now
             if (normal.y > 0.5f)
@@ -171,7 +208,7 @@ public class PlayerController : MonoBehaviour
         }
         if (collision.gameObject.CompareTag("Ice"))
         {
-            Vector3 normal = collision.contacts[0].normal;
+            //Vector3 normal = collision.contacts[0].normal;
             
             onIce = true;
             // Surface has to face up enough to be floor for now
@@ -180,11 +217,11 @@ public class PlayerController : MonoBehaviour
                 hasBurstCharge = true;
                 isGrounded = true;
             }
-            Debug.Log("ice");
+            // Debug.Log("ice");
         }
         if (collision.gameObject.CompareTag("Rubber"))
         {
-            Vector3 normal = collision.contacts[0].normal;
+            //Vector3 normal = collision.contacts[0].normal;
             
             onRubber = true;
             // Surface has to face up enough to be floor for now
@@ -193,11 +230,23 @@ public class PlayerController : MonoBehaviour
                 hasBurstCharge = true;
                 isGrounded = true;
             }
-            Debug.Log("rubber");
+            // Debug.Log("rubber");
+        }
+
+        if (collision.gameObject.CompareTag("Flypaper"))
+        {
+            //Vector3 normal = collision.contacts[0].normal;
+            stickyNormal = normal;
+            onSticky = true;
+            if (normal.y > 0.5f)
+            {
+                hasBurstCharge = true;
+                isGrounded = true;
+            }
+            // Debug.Log("Flypaper");
         }
     }
 
-    private int test = 0;
 
     private void OnCollisionExit(Collision collision)
     {
@@ -209,11 +258,17 @@ public class PlayerController : MonoBehaviour
         {
             onRubber = false;
         }
+
+        if (collision.gameObject.CompareTag("Flypaper"))
+        {
+            onSticky = false;
+            stickyNormal = Vector3.zero;
+        }
     }
 
     private void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground") ||  collision.gameObject.CompareTag("Ice") || collision.gameObject.CompareTag("Rubber"))
+        if (collision.gameObject.CompareTag("Ground") ||  collision.gameObject.CompareTag("Ice") || collision.gameObject.CompareTag("Rubber") || collision.gameObject.CompareTag("Flypaper"))
         {
             // Avoid double jump if we're still moving upwards from a jump
             if (rb.linearVelocity.y <= 0.1f)
