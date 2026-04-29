f432d5449c59e2172b2f599119cc2a6d37c40e0b
bmo-crayola <87660179+bmo-crayola@users.noreply.github.com>
2026-04-28 18:14:40 -0700

    Version1 Seemingly working

--- rollAway-project/Assets/Scripts/CheckpointMarker.cs
+++ rollAway-project/Assets/Scripts/CheckpointMarker.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class CheckpointMarker : MonoBehaviour
 {
-    
+    private Vector3 gravityDirection = Vector3.down;
+
+    public void SetGravityDirection(Vector3 newGravityDirection) {
+        gravityDirection = newGravityDirection;
+    }
+
+    public Vector3 GetGravityDirection() {
+        return gravityDirection;
+    }
 }
