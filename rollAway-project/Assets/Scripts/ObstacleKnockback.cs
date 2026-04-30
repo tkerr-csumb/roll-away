6cda82db615fbbb751c6c0bdcffc08042b52f6d5
bmo-crayola <87660179+bmo-crayola@users.noreply.github.com>
2026-04-30 01:00:37 -0700

    Current attempt (Not entirely working, but knockback left works ~80% of the time)

--- rollAway-project/Assets/Scripts/ObstacleKnockback.cs
+++ rollAway-project/Assets/Scripts/ObstacleKnockback.cs
@@ -2,29 +2,11 @@ using UnityEngine;
 
 public class ObstacleKnockback : MonoBehaviour
 {
-    private float knockbackForce = 10f;
-    private float upwardBias = 0.5f;
+    // Hit the player to the left with this force
+    private float knockbackForce = 50f;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.TryGetComponent<PlayerController>(out PlayerController player))
-        {
-            Vector3 contactPoint = collision.contacts[0].point;
-            ApplyKnockback(player, contactPoint);
-        }
-    }
-
-    private void ApplyKnockback(PlayerController player, Vector3 contactPoint)
-    {
-        Rigidbody playerRb = player.GetComponent<Rigidbody>();
-
-        if (playerRb != null)
-        {
-            Vector3 knockbackDirection = (player.transform.position - contactPoint).normalized;
-            knockbackDirection += Vector3.up * upwardBias;
-            knockbackDirection = knockbackDirection.normalized;
-            
-            playerRb.AddForce(knockbackDirection * knockbackForce, ForceMode.Impulse);
-        }
+        Debug.Break();
     }
 }
