afba30054877f92892d7bcd37e300e74a1a2c0ce
bmo-crayola <87660179+bmo-crayola@users.noreply.github.com>
2026-05-04 05:17:21 -0700

    Finished Swinging Obstacle working correctly

--- rollAway-project/Assets/Scripts/ObstacleSwingDriver.cs
+++ rollAway-project/Assets/Scripts/ObstacleSwingDriver.cs
@@ -5,18 +5,33 @@ public class ObstacleSwingDriver : MonoBehaviour
     private Rigidbody rb;
     private HingeJoint hinge;
 
+    public float startingAngle = 0f;
+
     [Header("Speed Settings")]
-    public float swingSpeed = 1.85f;
-    public float centerBoost = 1.75f;
+    public float swingSpeed = 4.05f;
+    public float centerBoost = 1.875f;
 
     private int direction = 1;
 
-    void Start()
+    // void Awake()
+    // {
+    //     rb = GetComponent<Rigidbody>();
+    //     hinge = GetComponent<HingeJoint>();
+
+    //     transform.localRotation = Quaternion.AngleAxis(startingAngle, hinge.axis);
+    // }
+
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
         hinge = GetComponent<HingeJoint>();
+
+        Quaternion spawnRotation = Quaternion.AngleAxis(startingAngle, hinge.axis);
         
-        rb.interpolation = RigidbodyInterpolation.Interpolate;
+        rb.position = transform.position; 
+        rb.rotation = transform.rotation * spawnRotation;
+
+        direction = (startingAngle >= 0) ? -1 : 1;
     }
 
     void FixedUpdate()
